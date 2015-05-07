0290c8d2e581cea1f2b61ab6f700315e2fbd7b0f
Brandon H <brandon@bc3tech.net>
2015-05-07 09:50:24 -0500

    Fix up naming/folder structure

--- DesktopApplicationInsights/Properties/AssemblyInfo.cs
+++ DesktopApplicationInsights/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinFormsApplicationInsights")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Desktop Application Insights")]
+[assembly: AssemblyDescription("Enables Microsoft Application Insights logging from Windows Desktop (Forms, Console, etc) applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinFormsApplicationInsights")]
+[assembly: AssemblyCompany("BC3 Technologies")]
+[assembly: AssemblyProduct("Desktop Application Insights")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyInformationalVersion("0.1-b1")]
+
