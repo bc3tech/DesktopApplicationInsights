50481cd153c4395be4cd73a73193262970e8fd29
Brandon H <brandon@bc3tech.net>
2015-05-11 19:48:45 -0500

    TelemetryButton refactor to Lazy<T>

--- DesktopApplicationInsights/TelemetryButton.cs
+++ DesktopApplicationInsights/TelemetryButton.cs
@@ -16,6 +16,49 @@ namespace DesktopApplicationInsights
     [DesignTimeVisible]
     public class TelemetryButton : Button
     {
+        private readonly Lazy<TelemetryClient> _telemetryClientFetcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TelemetryButton"/> class.
+        /// </summary>
+        public TelemetryButton() : base()
+        {
+            _telemetryClientFetcher = new Lazy<TelemetryClient>(() =>
+            {
+                System.Diagnostics.Debug.Assert(!string.IsNullOrWhiteSpace(this.TelemetryClientName),
+                    string.Format("No Telemetry client name set on Telemetry Button \"{0}\"", this.Name));
+
+                if (!string.IsNullOrWhiteSpace(this.TelemetryClientName))
+                {
+                    try
+                    {
+                        return Telemetry.GetClient(this.TelemetryClientName);
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.Fail(string.Format("Couldn't find telemetry client with name {0}",
+                            this.TelemetryClientName), ex.ToString());
+                    }
+                }
+
+                return null;
+            });
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the telemetry client used by the button
+        /// </summary>
+        [EditorBrowsable]
+        public string TelemetryClientName { get; set; }
+
+        /// <summary>
+        /// Gets the telemetry client.
+        /// </summary>
+        protected TelemetryClient TelemetryClient
+        {
+            get { return _telemetryClientFetcher.Value; }
+        }
+
         /// <summary>
         /// Gets or sets the Event Name to use when logging the Button's execution
         /// </summary>
@@ -30,46 +73,12 @@ namespace DesktopApplicationInsights
         public bool IsTimed { get; set; }
 
         /// <summary>
-        /// Gets or sets the name of the telemetry client used by the button
-        /// </summary>
-        [EditorBrowsable]
-        public string TelemetryClientName { get; set; }
-
-        private TelemetryClient _telemetryClient;
-        /// <summary>
-        /// Called when [create control].
-        /// </summary>
-        protected override void OnCreateControl()
-        {
-            if (!this.DesignMode)
-            {
-                System.Diagnostics.Debug.Assert(!string.IsNullOrWhiteSpace(this.TelemetryClientName),
-                    string.Format("No Telemetry client name set on Telemetry Button \"{0}\"", this.Name));
-
-                if (!string.IsNullOrWhiteSpace(this.TelemetryClientName))
-                {
-                    try
-                    {
-                        _telemetryClient = Telemetry.GetClient(this.TelemetryClientName);
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Diagnostics.Debug.Fail(string.Format("Couldn't find telemetry client with name {0}",
-                            this.TelemetryClientName), ex.ToString());
-                    }
-                }
-            }
-
-            base.OnCreateControl();
-        }
-
-        /// <summary>
         /// Raises the <see cref="E:Click" /> event.
         /// </summary>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected override void OnClick(EventArgs e)
         {
-            if (_telemetryClient != null)
+            if (this.TelemetryClient != null)
             {
                 var startTime = DateTime.UtcNow;
                 base.OnClick(e);
@@ -83,7 +92,8 @@ namespace DesktopApplicationInsights
                         telemetryData.Metrics.Add(string.Concat(this.EventName, "_Duration"),
                             (DateTime.UtcNow - startTime).TotalMilliseconds);
                     }
-                    _telemetryClient.TrackEvent(telemetryData);
+
+                    this.TelemetryClient.TrackEvent(telemetryData);
                 }
             }
         }
