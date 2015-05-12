110dff17d8f4e152356178689e5d69ee4d54525b
Brandon H <brandon@bc3tech.net>
2015-05-11 19:47:59 -0500

    TelemetryControl

---
+++ DesktopApplicationInsights/TelemetryControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ApplicationInsights;
+
+namespace DesktopApplicationInsights
+{
+    /// <summary>
+    /// Base control class with built-in Telemetry wireup
+    /// </summary>
+    public abstract class TelemetryControl : Control
+    {
+        private readonly Lazy<TelemetryClient> _telemetryClientFetcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TelemetryUserControl"/> class.
+        /// </summary>
+        protected TelemetryControl() : base()
+        {
+            _telemetryClientFetcher = new Lazy<TelemetryClient>(() =>
+            {
+                System.Diagnostics.Debug.Assert(!string.IsNullOrWhiteSpace(this.TelemetryClientName),
+                    string.Format("No Telemetry client name set on Telemetry Button \"{0}\"", this.Name));
+
+                if (!string.IsNullOrWhiteSpace(this.TelemetryClientName))
+                {
+                    try
+                    {
+                        return Telemetry.GetClient(this.TelemetryClientName);
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.Fail(string.Format("Couldn't find telemetry client with name {0}",
+                            this.TelemetryClientName), ex.ToString());
+                    }
+                }
+
+                return null;
+            });
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the telemetry client used by the button
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected abstract string TelemetryClientName { get; }
+
+        /// <summary>
+        /// Gets the telemetry client.
+        /// </summary>
+        protected TelemetryClient TelemetryClient
+        {
+            get { return _telemetryClientFetcher.Value; }
+        }
+    }
+}
